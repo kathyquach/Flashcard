1f992c0dc17419bdce2e048cf1783aeab9215fe7
Kathy Quach <kathyjquach@gmail.com>
2020-02-15 13:57:55 -0800

    Added in all necessary components

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -14,30 +14,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.presidentQ).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                (findViewById(R.id.presidentQ)).setVisibility(View.INVISIBLE);
+                (findViewById(R.id.presidentA)).setVisibility(View.VISIBLE);
+                (findViewById(R.id.answer_1)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+                (findViewById(R.id.answer_2)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+                (findViewById(R.id.answer_3)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+            }
+        });
+
+        findViewById(R.id.presidentA).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                (findViewById(R.id.presidentQ)).setVisibility(View.VISIBLE);
+                (findViewById(R.id.presidentA)).setVisibility(View.INVISIBLE);
+                (findViewById(R.id.answer_1)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+                (findViewById(R.id.answer_2)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+                (findViewById(R.id.answer_3)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+            }
+        });
+
         findViewById(R.id.answer_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((TextView)findViewById(R.id.answer_2)).setBackgroundColor(
-                        getResources().getColor(R.color.correct));
-                ((TextView)findViewById(R.id.answer_1)).setBackgroundColor(
+                (findViewById(R.id.answer_1)).setBackgroundColor(
                         getResources().getColor(R.color.incorrect));
+                (findViewById(R.id.answer_2)).setBackgroundColor(
+                        getResources().getColor(R.color.correct));
+                (findViewById(R.id.answer_3)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
             }
         });
 
         findViewById(R.id.answer_2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((TextView)findViewById(R.id.answer_2)).setBackgroundColor(
+                (findViewById(R.id.answer_1)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+                (findViewById(R.id.answer_2)).setBackgroundColor(
                         getResources().getColor(R.color.correct));
+                (findViewById(R.id.answer_3)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
             }
         });
 
         findViewById(R.id.answer_3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((TextView)findViewById(R.id.answer_2)).setBackgroundColor(
+                (findViewById(R.id.answer_1)).setBackgroundColor(
+                        getResources().getColor(R.color.yellow));
+                (findViewById(R.id.answer_2)).setBackgroundColor(
                         getResources().getColor(R.color.correct));
-                ((TextView)findViewById(R.id.answer_3)).setBackgroundColor(
+                (findViewById(R.id.answer_3)).setBackgroundColor(
                         getResources().getColor(R.color.incorrect));
             }
         });
